026cd3dfd595ce14cfee1b5c778ff5f7f29a4e82
nelsonbarros <nelson.barros@carambola.vc>
2022-09-26 00:38:21 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
+import React, { useReducer, useState } from 'react';
 import './App.css';
+import Title from "./components/Title.js";
+
+const formReducer = (state, event) => {
+  if(event.reset) {
+    return {
+      nome: '',
+      idade: 0,
+      estado: '',
+    }
+  }
+ return {
+   ...state,
+   [event.name]: event.value
+ }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [formData, setFormData] = useReducer(formReducer, {});
+  const [submitting, setSubmitting] = useState(false);
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setSubmitting(true);
+
+    setTimeout(() => {
+      setSubmitting(false);
+      setFormData({
+        reset: true
+      })
+    }, 3000);
+  }
+
+  const handleChange = event => {
+    setFormData({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
+
+  return(
+    <div className="wrapper">
+      <Title></Title>
+      {submitting &&
+               <div>
+               Seu Cadastro é:
+               <ul>
+                 {Object.entries(formData).map(([name, value]) => (
+                   <li key={name}><strong>{name}</strong>:{value.toString()}</li>
+                 ))}
+               </ul>
+             </div>
+      }
+      <form onSubmit={handleSubmit}>
+        <fieldset disabled={submitting}>
+          <label>
+            <p className='texto'>Nome</p>
+            <input type="text" name="nome" required onChange={handleChange} value={formData.nome || ''}/>
+          </label>
+        </fieldset>
+        <fieldset disabled={submitting}>
+         <label>
+           <p className='texto'>Selecione seu Estado</p>
+           <select name="estado" onChange={handleChange} required value={formData.estado || ''}>
+               <option value="">--Please choose an option--</option>
+               <option value="Rio de Janeiro">Rio de Janeiro</option>
+               <option value="São Paulo">São Paulo</option>
+               <option value="Minas Gerais">Minas Gerais</option>
+           </select>
+         </label>
+         <label>
+           <p className='texto'>Idade</p>
+           <input type="number" required name="idade" onChange={handleChange} step="1" value={formData.idade || ''}/>
+         </label>
+       </fieldset>
+        <button type="submit" disabled={submitting}>Submit</button>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
